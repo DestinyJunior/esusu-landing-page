59f6765d1ee18daf308ffac13e2755086b8444c3
Sunday Destiny <destinyjuniorsdj@gmail.com>
2021-02-17 18:50:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        14: "3.5rem",
+        18: "4.5rem",
+        68: "17.2rem",
+        82: "21.8rem",
+        88: "22.5rem",
+        98: "33rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
